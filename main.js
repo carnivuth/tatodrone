1a23655c10f3f71a8e3995b611ea83c7e3dcf6ed
carnivuth <matti200042@gmail.com>
2025-01-12 16:33:32 +0100

    added box

--- main.js
+++ main.js
@@ -7,6 +7,8 @@
 const DEBUG=true
 // DEBUG PARAMETER!!!!!!!!
 
+const bound = 100;
+
 function main(){
 
   try{
@@ -14,20 +16,32 @@ function main(){
     program = initProgram();
 
     // set drone at the origin, and give a default rotation to match axis
-    drone = new Drone(program, [ 0,10,0 ], [4.5,3.09,0],1);
-    tree = new Model(program,"assets/tree/tree.obj", [0,0,20],[0,0,0],0.5)
+    //drone = new Drone(program, [ 0,10,0 ], [4.5,3.09,0],1);
+    drone = new Drone(program, [ 0,10,0 ], [-90,0,0],1);
+    //tree = new Model(program,"assets/tree/tree.obj", [0,0,20],[0,0,0],0.5)
     floor = new Model(program,"assets/floor/floor.obj", [0,0,0],[0,0,0],1)
-    leftWall = new Model(program,"assets/wall/wall.obj", [-50,50,0],[0,0,90],1)
-    frontWall = new Model(program,"assets/wall/wall.obj", [0,50,50],[90,0,0],1)
-    backWall = new Model(program,"assets/wall/wall.obj", [0,50,-50],[90,0,0],1)
-    rightWall = new Model(program,"assets/wall/wall.obj", [50,50,0],[0,0,90],1)
+    leftWall = new Model(program,"assets/wall/wall.obj", [-bound,bound,0],[0,0,-90],1)
+    frontWall = new Model(program,"assets/wall/wall.obj", [0,bound,bound],[90,0,0],1)
+    backWall = new Model(program,"assets/wall/wall.obj", [0,bound,-bound],[-90,0,0],1)
+    rightWall = new Model(program,"assets/wall/wall.obj", [bound,bound,0],[0,0,90],1)
+    cubes = createCubes(10)
 
     // create light over the drone
     light = new Light(program,
       [
-          drone.position[0] +10,
-          drone.position[1]+ 10,
-          drone.position[2] + 10
+        drone.position[0] +10,
+        drone.position[1]+ 10,
+        drone.position[2] + 10
+      ],
+      [
+        0,
+        0,
+        0
+      ],
+      [
+        0.5,
+        0.5,
+        0.5
       ]
     );
 
@@ -35,12 +49,12 @@ function main(){
     camera = new Camera(program,
       // camera positions
       [
-          drone.position[0],
-          drone.position[1]+ 5,
-          drone.position[2] - 5
+        drone.position[0],
+        drone.position[1]+ 5,
+        drone.position[2] - 5
       ],
       drone.position,
-      [0,1,0],30);
+      [0,1,0],90);
 
     // creates debugView
     if(DEBUG){debugView = new DebugView(drone,light,camera);}
@@ -54,6 +68,9 @@ function main(){
       gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
       gl.enable(gl.DEPTH_TEST);
 
+      cubes.forEach((cube)=>{cube.rotateY(cube.rotation[1]+0.02)})
+
+
       //render objects
       camera.render()
       light.render()
@@ -64,13 +81,14 @@ function main(){
       backWall.render()
       leftWall.render()
       rightWall.render()
-      tree.render()
+      cubes.forEach((cube)=>{cube.render()})
+      //tree.render()
       drone.render()
 
       requestAnimationFrame(renderLoop);
-}
+    }
 
-  requestAnimationFrame(renderLoop);
+    requestAnimationFrame(renderLoop);
 
 
   }catch(e){
@@ -79,10 +97,21 @@ function main(){
 
 }
 
+function createCubes(nCubes){
+  cubes = []
+  for (var i =0; i<nCubes;i++){
+    xPosition=Math.floor(Math.random() * 80-40) ;
+    zPosition=Math.floor(Math.random() * 80-40);
+    cubes.push(new Model(program,"assets/cube/cube.obj", [xPosition,1,zPosition],[0,0,0],1))
+  }
+  return cubes;
+}
+
 function initWebGL(canvas_id){
 
   //get canvas DOM element
   var canvas = document.getElementById(canvas_id);
+  //canvas.setSize(window.innerWidth, window.innerHeight);
 
   // get webgl context
   if(DEBUG){
